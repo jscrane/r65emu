43c7aece2f71687a62b34137e2d9a719701cd1ec
Stephen Crane <jscrane@gmail.com>
2014-11-17 15:08:43 +0000

    utftdisplay is not a device on its own

--- utftdisplay.h
+++ utftdisplay.h
@@ -1,21 +1,14 @@
 #ifndef __UTFT_DISPLAY_H__
 #define __UTFT_DISPLAY_H__
 
-class Stream;
-
-class UTFTDisplay: public Memory::Device {
+class UTFTDisplay {
 public:
 	void begin(unsigned bg, unsigned fg);
 	void clear();
 	void error(char *);
 	void status(const char *);
 
-	virtual void checkpoint(Stream &s) = 0;
-	virtual void restore(Stream &s) = 0;
-
 protected:
-	UTFTDisplay(unsigned size): Memory::Device(size) {}
-
 	unsigned _bg, _fg, _cx, _cy, _dx, _dy, _oxs;
 };
 
