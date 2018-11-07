d80a4b0da2319a2168c1e27ea0fef0c7edf33aec
Stephen Crane <jscrane@gmail.com>
2018-11-07 21:37:20 +0000

    PROGMEM

--- prom.h
+++ prom.h
@@ -4,7 +4,7 @@
 class prom: public Memory::Device {
 public:
 	virtual void operator= (uint8_t) {}
-	virtual operator uint8_t () { return _mem[_acc]; }
+	virtual operator uint8_t () { return pgm_read_byte(_mem+_acc); }
 
 	prom(const uint8_t *mem, int uint8_ts): Memory::Device(uint8_ts), _mem(mem) {}
 
