72f7febe72da1709f8b11e35f400a981965b4c08
Stephen Crane <jscrane@gmail.com>
2018-11-08 07:34:28 +0000

    hardware override

--- hardware.h
+++ hardware.h
@@ -1,11 +1,12 @@
 /*
  * The hardware configuration of the machine.
- * (This should be the same for all emulated devices.)
  */
 #ifndef __HARDWARE_H__
 #define __HARDWARE_H__
 
-#if defined(ESP32)
+#if defined(HARDWARE_H)
+#include HARDWARE_H
+#elif defined(ESP32)
 #include "hw/esp32-utft-dac.h"
 #elif defined(ESP8266)
 #include "hw/esp8266-pwm.h"
