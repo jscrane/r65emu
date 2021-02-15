316405085b6968cd0b2412b8b0886d85e58e721e
Stephen Crane <jscrane@gmail.com>
2021-02-15 21:00:33 +0000

    esp8266 + SpiRAM

---
+++ hw/esp8266-pwm-fs-23k256.h
@@ -0,0 +1,32 @@
+// TFT display...
+#define USE_ESPI
+//must define these in Makefile (or platformio)
+//#define USER_SETUP_LOADED
+//#define ILI9341_DRIVER
+//#define TFT_CS	PIN_D8
+//#define TFT_DC	PIN_D1
+//#define TFT_WIDTH	240
+//#define TFT_HEIGHT	320
+//#define TFT_RST	-1
+//#define SPI_FREQUENCY	40000000
+//#define LOAD_GLCD
+
+// PS/2 keyboard
+#define KBD_DATA	D3
+#define KBD_IRQ		D4
+
+// SPI-RAM
+#define USE_SPIRAM
+#define SPIRAM_DEV	SPI
+#define SPIRAM_CS       D0
+#define SPIRAM_SIZE	32768
+
+// "tape" storage...
+#undef USE_SD
+#undef USE_SPIFFS
+#define USE_FS
+
+// sound
+#define PWM_SOUND	D2
+#define PWM_DUTY	20	// 20/1024 -> volume
+#define PWM_TOP		1024
