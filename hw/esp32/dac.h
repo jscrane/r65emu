0ad9cb174127df4348781bc4343e525a0195cdb0
Stephen Crane <jscrane@gmail.com>
2023-07-27 10:32:20 +0100

    just use tone()/noTone() to support PWM sound now (#17)

--- hw/esp32/dac.h
+++ hw/esp32/dac.h
@@ -1,4 +1,3 @@
 // sound: dac and pwm
 #define DAC_SOUND	25
 #define PWM_SOUND	25
-#define PWM_DUTY	20	// 20/1024 -> volume
--- hw/esp8266/pwm.h
+++ hw/esp8266/pwm.h
@@ -1,4 +1,3 @@
 // sound
 #define PWM_SOUND	D2
-#define PWM_DUTY	20	// 20/1024 -> volume
 #define PWM_TOP		1024
