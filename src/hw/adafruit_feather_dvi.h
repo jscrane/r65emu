ee9185f6c7ac03e8a9f1af2eb88e042c9b401437
steve <jscrane@gmail.com>
2024-10-12 09:23:03 +0100

    dvi

--- src/hw/adafruit_feather_dvi.h
+++ src/hw/adafruit_feather_dvi.h
@@ -2,7 +2,6 @@
 // https://www.adafruit.com/product/5710
 
 // FIXME:
-// #define USE_DVI
 // #define DAC_SOUND
 // #define PWM_SOUND
 
@@ -15,6 +14,18 @@
 #error "PS2_KBD_IRQ not defined"
 #endif
 #endif
+
+#define USE_DVI
+#if !defined(DVI_DEVICE)
+#define DVI_DEVICE		DVIGFX8
+#endif
+#if !defined(DVI_RESOLUTION)
+#define DVI_RESOLUTION		DVI_RES_320x240p60
+#endif
+#if !defined(DVI_DOUBLE_BUFFERED)
+#define DVI_DOUBLE_BUFFERED	false
+#endif
+#define DVI_CONFIG		adafruit_feather_dvi_cfg
 
 // 64kB RAM
 #define RAM_SIZE	0x10000u
