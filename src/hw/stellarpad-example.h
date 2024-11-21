24a8d844fd3d7c83af69f207cfa5d14b16576c8c
steve <jscrane@gmail.com>
2024-11-21 17:26:54 +0000

    add optional USE_OWN_DISPLAY

--- src/hw/stellarpad-example.h
+++ src/hw/stellarpad-example.h
@@ -30,6 +30,7 @@
 // TFT display...
 // NOTE: edit memorysaver.h to select the correct chip for your display!
 // Daniel Rebollo's boosterpack
+#if !defined(USE_OWN_DISPLAY)
 #define USE_UTFT
 #define TFT_BACKLIGHT	PD_6
 #define TFT_MODEL	SSD1289
@@ -37,4 +38,4 @@
 #define TFT_WR		PC_5
 #define TFT_CS		PC_7
 #define TFT_RST		PC_4
-
+#endif
