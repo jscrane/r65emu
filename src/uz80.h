21058244f24b60329f1435db93c329e5c671165d
steve <jscrane@gmail.com>
2025-11-11 09:30:21 +0000

    uz80 cleanups

--- src/uz80.h
+++ src/uz80.h
@@ -1,43 +1,47 @@
 #pragma once
 
+/**
+ * uz80 --- z80 CPU implementation based on Udo Munk's Arduino8080 implementation
+ * https://github.com/udo-munk/Arduino8080/
+ */
+
 // data types for the Z80 CPU
-typedef uint8_t  BYTE;
-typedef int8_t   SBYTE;
-typedef uint16_t WORD;
+typedef uint8_t		BYTE;
+typedef int8_t		SBYTE;
+typedef uint16_t	WORD;
 
 // bit definitions for the 8080 CPU flags
-#define S_FLAG          128
-#define Z_FLAG          64
-#define Y_FLAG          32
-#define H_FLAG          16
-#define X_FLAG          8
-#define P_FLAG          4
-#define N_FLAG          2
-#define C_FLAG          1
+#define S_FLAG	128
+#define Z_FLAG	64
+#define Y_FLAG	32
+#define H_FLAG	16
+#define X_FLAG	8
+#define P_FLAG	4
+#define N_FLAG	2
+#define C_FLAG	1
 
-#define S_SHIFT         7
-#define Z_SHIFT         6
-#define Y_SHIFT         5
-#define H_SHIFT         4
-#define X_SHIFT         3
-#define P_SHIFT         2
-#define N_SHIFT         1
-#define C_SHIFT         0
+#define S_SHIFT	7
+#define Z_SHIFT 6
+#define Y_SHIFT 5
+#define H_SHIFT 4
+#define X_SHIFT 3
+#define P_SHIFT 2
+#define N_SHIFT 1
+#define C_SHIFT 0
 
-// possible states of the 8080 CPU
 enum CPUState { Running = 1, Halted = 2, Interrupted = 3 };
 
 class uz80: public CPU {
 public:
 	uz80(Memory &m): CPU(m) {}
 
-        void run(unsigned);
+	void run(unsigned);
 	void reset();
 	void irq(uint8_t b);
 	char *status(char *buf, size_t n, bool hdr = false);
 
-	void checkpoint(Stream &) {}
-	void restore(Stream &) {}
+	void checkpoint(Stream &);
+	void restore(Stream &);
 
 	void set_port_out_handler(std::function<void(uint16_t, uint8_t)> fn) {
 		port_out_handler = fn;
@@ -52,56 +56,55 @@ private:
 	uint8_t int_data, int_mode, int_protection;
 	byte _handle_interrupt();
 
-        std::function<void(uint16_t, uint8_t)> port_out_handler;
-        std::function<uint8_t(uint16_t)> port_in_handler;
+	std::function<void(uint16_t, uint8_t)> port_out_handler;
+	std::function<uint8_t(uint16_t)> port_in_handler;
 
 #if _BYTE_ORDER == _LITTLE_ENDIAN
 	struct cpu_reg {
-	  union {
-	    struct {
-	      BYTE l;
-	      BYTE h;
-	    };
-	    WORD w;
-	  };
+		union {
+			struct {
+				BYTE l;
+				BYTE h;
+			};
+			WORD w;
+		};
 	};
 #elif _BYTE_ORDER == _BIG_ENDIAN
 	struct cpu_reg {
-	  union {
-	    struct {
-	      BYTE h;
-	      BYTE l;
-	    };
-	    WORD w;
-	  };
+		union {
+			struct {
+				BYTE h;
+				BYTE l;
+			};
+			WORD w;
+		};
 	};
 #else
 #error "Unsupported byte order"
 #endif
 
-	// Z80 CPU state
 	struct cpu_state {
-	  struct cpu_reg af;    /* primary registers */
-	  struct cpu_reg bc;
-	  struct cpu_reg de;
-	  struct cpu_reg hl;
-	  struct cpu_reg sp;    /* stack pointer */
-	  struct cpu_reg pc;    /* program counter */
-	  struct cpu_reg af_;   /* Z80 alternate registers */
-	  struct cpu_reg bc_;
-	  struct cpu_reg de_;
-	  struct cpu_reg hl_;
-	  struct cpu_reg ix;    /* Z80 index registers */
-	  struct cpu_reg iy;
-	  BYTE i;               /* Z80 interrupt register */
-	  BYTE r;               /* Z80 refresh register (7-bit counter) */
-	  BYTE r_;              /* 8th bit of R (can be loaded with LD R,A) */
-	  BYTE iff;             /* interupt flags */
+		struct cpu_reg af;	/* primary registers */
+		struct cpu_reg bc;
+		struct cpu_reg de;
+		struct cpu_reg hl;
+		struct cpu_reg sp;	/* stack pointer */
+		struct cpu_reg pc;	/* program counter */
+		struct cpu_reg af_;	/* Z80 alternate registers */
+		struct cpu_reg bc_;
+		struct cpu_reg de_;
+		struct cpu_reg hl_;
+		struct cpu_reg ix;	/* Z80 index registers */
+		struct cpu_reg iy;
+		BYTE i;			/* Z80 interrupt register */
+		BYTE r;			/* Z80 refresh register (7-bit counter) */
+		BYTE r_;		/* 8th bit of R (can be loaded with LD R,A) */
+		BYTE iff;		/* interupt flags */
 	} cpu_regs;
 
 	// other global variables
-	CPUState State = Running;         // CPU state
-	unsigned long tstates = 0;        // executed T-states
+	CPUState State = Running;	// CPU state
+	unsigned long tstates = 0;	// executed T-states
 	
 	// z80sim compatibility
 	inline BYTE memrdr(WORD addr) { return _mem[addr]; }
