e03404d0d727abbb8e4d14cf30a5e82fff04360a
steve <jscrane@gmail.com>
2026-04-24 18:02:24 +0100

    bugfix

--- src/spiram.cpp
+++ src/spiram.cpp
@@ -18,23 +18,25 @@ uint8_t spiram::get(Memory::address a)
 	return spiRam.read_byte(a);
 }
 
-void spiram::checkpoint(Checkpoint &c, Memory::address start, size_t len)
+void spiram::checkpoint(Checkpoint &c, Memory::address addr, size_t len)
 {
 	uint8_t buf[256];
 	unsigned pages = len / sizeof(buf);
 	for (unsigned i = 0; i < pages; i++) {
-		spiRam.read_stream((start + i) * sizeof(buf), buf, sizeof(buf));
+		spiRam.read_stream(addr, buf, sizeof(buf));
 		c.write(buf, sizeof(buf));
+		addr += sizeof(buf);
 	}
 }
 
-void spiram::restore(Checkpoint &c, Memory::address start, size_t len)
+void spiram::restore(Checkpoint &c, Memory::address addr, size_t len)
 {
 	uint8_t buf[256];
 	unsigned pages = len / sizeof(buf);
 	for (unsigned i = 0; i < pages; i++) {
 		c.read(buf, sizeof(buf));
-		spiRam.write_stream((start + i) * sizeof(buf), buf, sizeof(buf));
+		spiRam.write_stream(addr, buf, sizeof(buf));
+		addr += sizeof(buf);
 	}
 }
 
