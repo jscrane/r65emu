24d26438f2710b8af9f867e08ce0da161b3ebcb1
steve <jscrane@gmail.com>
2026-08-11 18:13:17 +0100

    allow spec of page_bits and device_bits indept of address_bits

--- src/memory.h
+++ src/memory.h
@@ -13,6 +13,14 @@ public:
 #define MEMORY_ADDRESS_WIDTH 16
 #endif
 
+#ifndef MEMORY_PAGE_WIDTH
+#define MEMORY_PAGE_WIDTH (MEMORY_ADDRESS_WIDTH / 2)
+#endif
+
+#ifndef MEMORY_DEVICE_WIDTH
+#define MEMORY_DEVICE_WIDTH (MEMORY_ADDRESS_WIDTH / 4)
+#endif
+
 class Memory: public Checkpointable {
 public:
 	typedef uint32_t address;
@@ -20,7 +28,7 @@ public:
 	static_assert(MEMORY_ADDRESS_WIDTH % 4 == 0, "address_bits must be divisible by 4");
 
 	static const unsigned address_bits = MEMORY_ADDRESS_WIDTH;
-	static const unsigned page_bits = address_bits / 2;
+	static const unsigned page_bits = MEMORY_PAGE_WIDTH;
 
 	static const size_t address_size = (1 << address_bits);
 	static const size_t page_size = (1 << page_bits);
@@ -58,7 +66,7 @@ public:
 
 	class Devices: public Device {
 	public:
-		static const unsigned device_bits = address_bits / 4;
+		static const unsigned device_bits = MEMORY_DEVICE_WIDTH;
 		static const size_t device_size = (1 << device_bits);
 
 		Devices(): Device(page_size), _nd(page_size) {
