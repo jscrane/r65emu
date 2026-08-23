69c7a00073beae75619183ec79eb8aed0a9f04ca
steve <jscrane@gmail.com>
2026-08-23 09:10:24 +0100

    update

--- src/m68008.cpp
+++ src/m68008.cpp
@@ -22,10 +22,46 @@ void m68008::run(unsigned clocks) {
 	}
 }
 
-void m68008::checkpoint(Checkpoint &) {
-	// FIXME
+void m68008::checkpoint(Checkpoint &c) {
+	c.write(_d[0]);
+	c.write(_d[1]);
+	c.write(_d[2]);
+	c.write(_d[3]);
+	c.write(_d[4]);
+	c.write(_d[5]);
+	c.write(_d[6]);
+	c.write(_d[7]);
+	c.write(_a[0]);
+	c.write(_a[1]);
+	c.write(_a[2]);
+	c.write(_a[3]);
+	c.write(_a[4]);
+	c.write(_a[5]);
+	c.write(_a[6]);
+	c.write(_a[7]);
+	c.write(_usp);
+	c.write(_ssp);
+	c.write(_sr);
 }
 
-void m68008::restore(Checkpoint &) {
-	// FIXME
+void m68008::restore(Checkpoint &c) {
+	c.read(_d[0]);
+	c.read(_d[1]);
+	c.read(_d[2]);
+	c.read(_d[3]);
+	c.read(_d[4]);
+	c.read(_d[5]);
+	c.read(_d[6]);
+	c.read(_d[7]);
+	c.read(_a[0]);
+	c.read(_a[1]);
+	c.read(_a[2]);
+	c.read(_a[3]);
+	c.read(_a[4]);
+	c.read(_a[5]);
+	c.read(_a[6]);
+	c.read(_a[7]);
+	c.read(_usp);
+	c.read(_ssp);
+	c.read(_sr);
 }
