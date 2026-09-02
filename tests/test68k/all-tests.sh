1cdaa9371fa03924a65a03786a9688f98b94d51c
steve <jscrane@gmail.com>
2026-09-02 13:37:42 +0100

    rudimentary regression test harness

---
+++ tests/test68k/all-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TESTDIR=~/src/680x0/68000/v1
+TESTS="ADD.b ADDX.b AND.b ANDItoCCR ANDItoSR Bcc RESET"
+
+for t in $TESTS; do
+	echo -n "$t: "
+	./sstests ${TESTDIR}/$t.json.gz -k > /tmp/$$.tmp
+	if $(cmp -s /tmp/$$.tmp results/$t); then
+		echo "PASS"
+	else
+		echo "FAIL"
+		exit -1
+	fi
+done
